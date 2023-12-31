1d9a46aa64d571455cd9835d366381821f74da3c
Hendrik Wichern <git@hendrikwichern.de>
2023-12-31 13:36:13 +0100

    added generative favicon

---
+++ apps/web/src/routes/favicon/+server.ts
@@ -0,0 +1,33 @@
+import { error, type RequestHandler } from '@sveltejs/kit';
+import { image_from_component, type RenderOptions } from 'svelte-component-to-image';
+
+
+import Favicon from './Favicon.svelte';
+
+export const GET: RequestHandler = (async ({ url }: { url: URL }) => {
+  try {
+    const options: RenderOptions = {
+      width: 128,
+      height: 128,
+      props: {
+        text: url.searchParams.get('text') ?? 'text not found'
+      },
+      fonts: [
+        {
+          name: 'Typewriter',
+          url: `${url.origin}/fonts/Noto_Emoji/static/NotoEmoji-Bold.ttf`,
+        }
+      ]
+    }
+
+    // pass the component and options to the package
+    const image = await image_from_component(Favicon, options)
+    const response = new Response(image)
+    response.headers.append('Content-Type', 'image/png')
+    response.headers.append('Cache-Control', 's-maxage=604800, stale-while-revalidate=604800')
+    return response
+  } catch (e) {
+    console.error(e)
+    throw error(500, 'Error trying to generate image from component.')
+  }
+}) satisfies RequestHandler
---
+++ apps/web/src/routes/favicon/Favicon.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+  const characters = [
+    '👋',
+    '🤚',
+    '✋',
+    '🖖',
+    '👌',
+    '🤌',
+    '✌️',
+    '🤞',
+    '🫰',
+    '🤟',
+    '🤘',
+    '🤙',
+    '👍',
+    '✊',
+    '🫶',
+    '🙌',
+    '👐',
+    '🙏',
+    '✍️',
+    '💅',
+    '💪',
+    '👾',
+    '🤖',
+    '👽',
+    '🤠',
+    '💫',
+    '✨',
+    '⭐️',
+    '⚡️',
+    '🎨',
+    '🧨',
+    '🎊',
+    '❤️‍🔥',
+    '💘'
+  ];
+  const char = characters[Math.floor(Math.random() * characters.length)];
+
+  function rand(min: number, max: number) {
+    return parseInt((Math.random() * (max - min + 1)).toString(), 10) + min;
+  }
+
+  function get_random_color() {
+    var h = rand(200, 360);
+    var s = rand(54, 75);
+    var l = rand(45, 55);
+    return 'hsl(' + h + ',' + s + '%,' + l + '%)';
+  }
+
+  const randomColor = get_random_color();
+  const randomRotation = (Math.random() - 0.5) * 90; // rotate between -45 to 45 degrees
+</script>
+
+<div
+  style="background-color: {randomColor}; transform: translate(-50%, -50%) rotate({randomRotation}deg)"
+  class="box"
+>
+  {char}
+</div>
+
+<style>
+  .box {
+    position: absolute;
+    width: 68%;
+    height: 68%;
+    top: 50%;
+    left: 50%;
+    color: white;
+    font-size: 90px;
+    line-height: 90px;
+  }
+</style>
